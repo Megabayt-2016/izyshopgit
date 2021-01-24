9c0c77be0acc6a6507288a429b39418837ba18ac
Megabay <nrd.laaraj@gmail.com>
2021-01-24 09:26:25 +0100

    Update slider-homepage.php
    change slide text

--- template-items/slider-homepage.php
+++ template-items/slider-homepage.php
@@ -9,7 +9,7 @@
             <img src="<?php bloginfo('template_url') ?>/img/s-1.jpg" alt="" class="img-fluid">
 
             <div class="slider-info" >
-                <h2 data-aos="filp-up" data-aos-delay="300">Izy shop</h2>
+                <h2 data-aos="filp-up" data-aos-delay="300">I'm Still working in the backend in website .. Thanks for selecting the 3products</h2>
             </div>
             </div>
         </div>            
